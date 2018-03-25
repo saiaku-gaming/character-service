106ab5d52bdeb6abae4e0cffe833cc1acf9259d0
Mikael Berglund <mikbe@kth.se>
2018-03-25 20:03:14 +0200

    select character now takes a display character name
    as that was easier to get in unreal.

--- src/main/java/com/valhallagame/characterserviceclient/message/SelectCharacterParameter.java
+++ src/main/java/com/valhallagame/characterserviceclient/message/SelectCharacterParameter.java
@@ -13,8 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SelectCharacterParameter {
 	@NotNull
-	@CheckLowercase
-	private String characterName;
+	private String displayCharacterName;
 	@NotNull
 	@CheckLowercase
 	private String username;
