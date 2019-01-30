79151f2c29fc6b03d9e7d9d5b0c7839daf4f57b8
Mio Engel <mioengel@gmail.com>
2019-01-30 20:58:53 +0100

    Adds more armor types

--- src/main/java/com/valhallagame/characterserviceclient/model/CharacterData.java
+++ src/main/java/com/valhallagame/characterserviceclient/model/CharacterData.java
@@ -1,12 +1,11 @@
 package com.valhallagame.characterserviceclient.model;
 
-import javax.validation.constraints.NotNull;
-
-import org.hibernate.validator.constraints.NotBlank;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
@@ -21,9 +20,24 @@ public class CharacterData {
 	
 	@NotBlank
 	private String characterName;
-	
+
+	@NotNull
+	private String headItem;
+
+	@NotNull
+	private String beardItem;
+
 	@NotNull
     private String chestItem;
+
+	@NotNull
+	private String handsItem;
+
+	@NotNull
+	private String legsItem;
+
+	@NotNull
+	private String feetItem;
 
 	@NotNull
     private String mainhandArmament;
